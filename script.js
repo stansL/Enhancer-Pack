9e30f9686f7dd6d69cbb5f4a817a556f85cbf65c
Stanslaus <mstans8@gmail.com>
2019-12-05 19:36:11 +0300

    intro to arrow functions for the tutorial files

--- script.js
+++ script.js
@@ -36,4 +36,48 @@ driversLicense6(true);
     let b = 45;
     console.log(a + b);
 }
-console.log(a + b);
+
+
+
+let surName = 'Kamau';
+let lastName = 'Njuguna';
+const yob = 1987;
+
+function calcAGe(year) {
+    return 2019 - year;
+}
+
+
+console.log('This is  ' + surName + ' ' + lastName + ' and he was born in ' + yob + '. He is ' + calcAGe(yob) + ' years old');
+//Template literals version
+console.log(`This is ${surName} ${lastName} and he was born in ${yob}. He is ${calcAGe(yob)} years old!`);
+
+//More string methods
+const nameSix = `${surName} ${lastName}`;
+
+console.log(nameSix.startsWith('k'));
+console.log(nameSix.endsWith('a'));
+
+
+console.log(nameSix.includes('gun'));
+console.log(nameSix.repeat(5));
+
+/**
+ *
+ * Arrow Functions
+ */
+
+const years = [1982, 1956, 1972, 1975, 1987];
+
+//ES5
+var computedAges = years.map(function (el) {
+    return 2019 - el;
+
+});
+console.log(computedAges);
+
+//ES6
+const newAges = years.map(e => { //when more than one line of code
+    return 2019 - e
+});
+console.log(newAges);
