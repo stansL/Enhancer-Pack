a83aa2544fac54783f1e481834289c4479e2c448
Stanslaus <mstans8@gmail.com>
2019-12-10 09:38:10 +0300

    update inheritance using ES6

--- classes.js
+++ classes.js
@@ -36,3 +36,39 @@ const tPerson = new Person('John', 1987, 'Software Engineer');
 console.log(tPerson);
 
 Person.greeting();
+
+
+//Subclasses in ES5
+
+
+var AthleteTest = function (name, yearOfBirth, job, olympicGames, medals) {
+    PersonTest.call(this, name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+}
+
+AthleteTest.prototype = Object.create(PersonTest.prototype);
+AthleteTest.prototype.wonMedal = function () {
+    this.medals++;
+    console.log(this.medals);
+}
+
+var john = new AthleteTest('Stanslaus', 1987, 'Software Engineer', 3, 10);
+
+
+
+//ES6 Subclasses.
+class Athlete extends Person {
+    constructor(name, yearOfBirth, job, olympicGames, medals) {
+        super(name, yearOfBirth, job);
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+
+    wonMedal() {
+        this.medals++;
+        console.log(this.medals);
+    }
+}
+
+var athleteTest = new Athlete('Peter', 1989, 'Swimmer', 3, 1)
