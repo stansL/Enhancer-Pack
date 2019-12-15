aa453fecff22aa4fa2cf70b30c23af6439a0d99a
Stanslaus <mstans8@gmail.com>
2019-12-15 22:11:15 +0300

    add sections on the event loop

--- async.js
+++ async.js
@@ -1,7 +1,9 @@
 const second = () => {
+    console.log("Just inside the second function....");
     setTimeout(() => {
         console.log("Async Hi there");
-    }, 4000)
+    }, 4000);
+    console.log("Last line on the second function...");
 }
 const first = () => {
     console.log('Look into ES6 syntax and async stuff');
